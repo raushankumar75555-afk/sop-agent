2f4d05a0b208eb064b523fb2c1fdbf5a557b8897
raushankumar75555-afk <raushankumar75555@gmail.com>
2026-06-25 14:39:38 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -81,7 +81,7 @@ export default function App() {
   return (
     <div className="flex h-screen bg-slate-50 dark:bg-slate-950">
       <Sidebar activeTab={activeTab} onTabChange={setActiveTab} />
-      <main className="flex-1 flex flex-col overflow-hidden">
+      <main className="flex-1 flex flex-col min-h-0 overflow-hidden">
         {renderPage()}
       </main>
     </div>
